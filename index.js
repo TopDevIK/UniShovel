d5253d4cfc4a900022c9f07bb8e2c2000bfda6a1
OzoneClub <aceofcryto.com>
2022-06-04 05:51:52 +0000

    add exception handle with web3 & remove unused functions

--- index.js
+++ index.js
@@ -41,27 +41,6 @@ const parseArgument = (_prefix, _longPrefix, _defaultValue) => {
     return result;
 };
 
-const getBlockNumberByTimeStamp = async (timeStamp) => {
-    let i = 0;
-    while (i < 5)
-        try {
-            const response = await fetch(
-                `https://api.bscscan.com/api?module=block&action=getblocknobytime&timestamp=${timeStamp}&closest=before&apikey=${BSCSCAN_API_KEY}`
-            );
-            const data = await response.json();
-            console.log(data);
-            return data.result;
-        } catch (error) {
-            console.log(
-                timeStamp,
-                `bscscan api error with timesamp :try again ${i} times,`,
-                error
-            );
-            i++;
-        }
-    process.exit();
-};
-
 const getBlockNumberByTimeStampGraphQL = async (timeStamp, index) => {
     const BITQUERY_API_KEY = CURRENT_API_KEY_ARRAY.at(parseInt(index));
     let text;
@@ -117,53 +96,32 @@ const getBlockNumberByTimeStampGraphQL = async (timeStamp, index) => {
     process.exit();
 };
 const getPriceDuringPeriodWeb3js = async (fromBlock, toBlock) => {
-    let logs = await web3.eth.getPastLogs({
-        address: pairAddress,
-        fromBlock,
-        toBlock,
-        topics: [
-            "0xd78ad95fa46c994b6551d0da85fc275fe613ce37657fb8d5e3d130840159d822",
-        ],
-    });
-    if (logs.length === 0) return undefined;
-    const item = logs[0];
-    amount0In = parseInt("0x" + item.data.slice(2, 66));
-    amount0Out = parseInt("0x" + item.data.slice(130, 194));
-    amount1In = parseInt("0x" + item.data.slice(66, 130));
-    amount1Out = parseInt("0x" + item.data.slice(194));
-
-    usdPrice = (amount1In + amount1Out) / (amount0In + amount0Out);
-
-    return usdPrice;
-};
-const getPriceDuringPeriod = async (fromBlock, toBlock) => {
-    let usdPrice;
     let i = 0;
-    while (i < 5)
+    while (i < 5) {
         try {
-            let response = await fetch(
-                `https://api.bscscan.com/api?module=logs&action=getLogs&fromBlock=${fromBlock}&toBlock=${toBlock}&address=${pairAddress}&topic0=0xd78ad95fa46c994b6551d0da85fc275fe613ce37657fb8d5e3d130840159d822&apikey=${BSCSCAN_API_KEY}`
-            );
-            const data = await response.json();
-            if (response.status === 200) {
-                if (data.result.length === 0) return undefined;
-                const item = data.result[0];
-                amount0In = parseInt("0x" + item.data.slice(2, 66));
-                amount0Out = parseInt("0x" + item.data.slice(66, 130));
-                amount1In = parseInt("0x" + item.data.slice(130, 194));
-                amount1Out = parseInt("0x" + item.data.slice(194));
+            let logs = await web3.eth.getPastLogs({
+                address: pairAddress,
+                fromBlock,
+                toBlock,
+                topics: [
+                    "0xd78ad95fa46c994b6551d0da85fc275fe613ce37657fb8d5e3d130840159d822",
+                ],
+            });
+            if (logs.length === 0) return undefined;
+            const item = logs[0];
+            amount0In = parseInt("0x" + item.data.slice(2, 66));
+            amount0Out = parseInt("0x" + item.data.slice(130, 194));
+            amount1In = parseInt("0x" + item.data.slice(66, 130));
+            amount1Out = parseInt("0x" + item.data.slice(194));
 
-                usdPrice = (amount0In + amount0Out) / (amount1In + amount1Out);
+            usdPrice = (amount1In + amount1Out) / (amount0In + amount0Out);
 
-                return usdPrice;
-            }
+            return usdPrice;
         } catch (error) {
-            console.log(
-                `bscscan api error with getting log ${fromBlock} - ${toBlock}:try again ${i} times`,
-                error
-            );
+            console.log(`web3 error ${fromBlock}`, error);
             i++;
         }
+    }
     process.exit();
 };
 
